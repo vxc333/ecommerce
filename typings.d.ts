6c095b44d52ae329dbae73640c93fd98daa54be5
vxc333 <you@example.com>
2024-08-25 16:28:00 -0300

    creating product and productImage

---
+++ typings.d.ts
@@ -0,0 +1,13 @@
+type Product = {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: Rating;
+};
+type Rating = {
+  rate: number;
+  count: number;
+}
